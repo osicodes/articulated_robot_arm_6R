689cfc46485a1fccc6b34bb554fc48ad71bb6b34
MECT1 <MECT1@192.168.168.7>
2024-08-04 22:39:26 +0300

    scara progress made

--- ScrewIK_SCARA.m
+++ ScrewIK_SCARA.m
@@ -40,7 +40,7 @@ sv(:,4) = v4;
 
 s = [sw;sv];
 
-qr = [pi/6; pi/3; 0; 0];
+qr = [-pi/6; -pi/3; pi; 30];
 
 g0 = [1  0  0 a1+a2;
      0 -1  0     0;
@@ -84,16 +84,30 @@ th2v_prime = th2V - th2omega*th2omega'*th2V;
 
 theta_not = atan2(th2omega'*Vector3Cross(th2u_prime,th2v_prime)',...
     th2u_prime'*th2v_prime);
+% theta_not = atan((th2omega'*Vector3Cross(th2u_prime,th2v_prime)')/(th2u_prime'*th2v_prime));
+% theta_not * 180/pi
 
 
 phi_num = round(norm(th2U)^2 + norm(th2V)^2 - delta_prime_square, 4);
 phi_den = round(2 * norm(th2U) * norm(th2V), 4);
 phi = acos(phi_num/phi_den);
+% phi * 180/pi
+
+P04=gst(1:3,4);
+P02 = P04 - a2*gst(1:3,1);
 
 if (target(1) > 0 && target(2) > 0)
-    theta2 = (theta_not - phi);
+    if (target(2) > P02(2))
+        theta2 = (theta_not - phi);
+    elseif (target(2) < P02(2))
+        theta2 = (-1*theta_not + phi);
+    end
 elseif (target(1) > 0 && target(2) < 0)
-    theta2 = (theta_not - phi);
+    if (target(2) > P02(2))
+        theta2 = (theta_not - phi);
+    elseif (target(2) < P02(2))
+        theta2 = (-1*theta_not + phi);
+    end
 end
 
 theta2_d = theta2 *180/pi
@@ -116,11 +130,39 @@ th1u_prime = th1U - th1omega*th1omega'*th1U;
 th1v_prime = th1V - th1omega*th1omega'*th1V;
 
 theta1 = round(atan((th1omega'*Vector3Cross(th1u_prime,th1v_prime)')/...
-    (th1u_prime'*th1v_prime)),4)
+    (th1u_prime'*th1v_prime)),4);
 
 theta1_d = theta1 * 180/pi
 
 
+%---------theta3-----------
+% Apply subproblem 1
+exp1 = MatrixExp6(VecTose3(s(:,1) * theta1));
 
+th3p_T = inv(exp2) * inv(exp1) * g1 * [p2;1];
+th3p = th3p_T(1:3);
+th3q = target(1:3);
+th3r = p3;
+th3omega = sw(:,3);
 
+th3U = th3p - th3r;
+th3V = th3q - th3r;
 
+th3u_prime = th3U - th3omega*th3omega'*th3U;
+th3v_prime = th3V - th3omega*th3omega'*th3V;
+
+theta3 = round(atan((th3omega'*Vector3Cross(th3u_prime,th3v_prime)')/...
+    (th3u_prime'*th3v_prime)),4);
+
+theta3_d = theta3 * 180/pi
+
+% -------validate-----
+
+qr_new = [theta1; theta2; theta3; d3];
+
+g0 = [1  0  0 a1+a2;
+     0 -1  0     0;
+     0  0 -1    d0;
+     0  0  0     1];
+
+gst_new = FKinSpace(g0, s, qr_new)
